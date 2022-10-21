b7e2c04071070efe51d4905deb582c3d370ed5f2
Federica Legger <federica.legger@cern.ch>
2022-10-21 09:40:07 +0000

    add install.sh and Day3 notebooks

---
+++ Notebooks/Day1/install.sh
@@ -0,0 +1,9 @@
+# Install script
+
+# for notebook
+pip3 install pandas seaborn pyarrow fastparquet sklearn
+pip3 install tensorflow
+
+# for minio
+pip3 install liboidcagent requests xmltodict boto3
+
